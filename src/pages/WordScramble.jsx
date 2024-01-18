829209faad11ab9e9a426342c3a943ec6bf82b36
Dipa Galatian <dipagalatian3003@gmail.com>
2024-01-18 23:06:46 +0700

    finish WordScramble page

--- src/pages/WordScramble.jsx
+++ src/pages/WordScramble.jsx
@@ -1,10 +1,78 @@
 import { Link } from "react-router-dom";
 import "../css/wordscramble.css";
+import { useEffect, useState } from "react";
+
+const WORDS = ["singa", "gajah", "kuda", "kucing", "zebra", "anjing", "buaya", "kelinci"];
 
 const WordScramble = () => {
-    const handleEnter = () => {
-        console.log("clicked");
+    const [inputValue, setInputValue] = useState("");
+    const [message, setMessage] = useState("");
+    const [isPlay, setIsPlay] = useState(false);
+    const [correctWord, setCorrectWord] = useState("");
+    const [scrambleWord, setScrambleWord] = useState("");
+
+    useEffect(() => {
+        let clearMessage;
+        if (message === "Wrong Answer!") {
+            clearMessage = setTimeout(() => setMessage(""), 2000);
+        }
+        return () => {
+            if (clearMessage) {
+                clearTimeout(clearMessage);
+            }
+        };
+    }, [message]);
+
+    const selectWord = () => {
+        const randomIndex = Math.floor(Math.random() * WORDS.length);
+        return WORDS[randomIndex];
     };
+
+    const handleEnter = () => {
+        if (inputValue) {
+            if (correctWord.toLowerCase() === inputValue.toLowerCase()) {
+                setMessage("Correct Answer!");
+            } else {
+                setMessage("Wrong Answer!");
+            }
+        } else {
+            alert("You have to guest the word");
+        }
+    };
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleStartGame = () => {
+        setIsPlay(true);
+        const word = selectWord();
+        setCorrectWord(word);
+        console.log("word:", word);
+        setScrambleWord(construcScrambleWord(word));
+    };
+
+    const handleNewGame = () => {
+        setInputValue("");
+        setMessage("");
+        const word = selectWord();
+        setCorrectWord(word);
+        console.log("word:", word);
+        setScrambleWord(construcScrambleWord(word));
+    };
+
+    const construcScrambleWord = (word) => {
+        const shuffleWord = word.split("");
+        //The Fisher-Yates Suffle method
+        for (let i = shuffleWord.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let k = shuffleWord[i];
+            shuffleWord[i] = shuffleWord[j];
+            shuffleWord[j] = k;
+        }
+        return shuffleWord.join("");
+    };
+    console.log("inputValuestate:", inputValue);
 
     return (
         <>
@@ -13,20 +81,31 @@ const WordScramble = () => {
             </Link>
             <h1 className="header">Word Scramble</h1>
             <div className="word-container-all">
-                <div className="message">wrong answer</div>
-                <div className="word">
-                    <span>T</span>
-                    <span>E</span>
-                    <span>S</span>
-                    <span>T</span>
-                </div>
-                <div className="input-word">
-                    <input type="text" placeholder="Guest the word" />
-                    <button className="btn-enter-word" onClick={handleEnter}>
-                        Enter
+                {!!message && <div className="message">{message}</div>}
+
+                {isPlay ? (
+                    <>
+                        <div className="word">
+                            {correctWord.split("").map((item, i) => (
+                                <span key={i}>{inputValue[i]}</span>
+                            ))}
+                        </div>
+                        <p className="scramble-word">{scrambleWord}</p>
+                        <div className="input-word">
+                            <input type="text" placeholder="Guest the word" onChange={handleInputChange} value={inputValue} />
+                            <button className="btn-enter-word" onClick={handleEnter}>
+                                Enter
+                            </button>
+                        </div>
+                        <button className="btn-start-game" onClick={handleNewGame}>
+                            NEW GAME
+                        </button>
+                    </>
+                ) : (
+                    <button className="btn-start-game" onClick={handleStartGame}>
+                        START GAME
                     </button>
-                </div>
-                <button className="btn-start-game">Start Game</button>
+                )}
             </div>
         </>
     );
