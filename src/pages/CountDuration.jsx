fd986d70c08017ba41a4f8475b33a0b72961fc21
Dipa Galatian <dipagalatian3003@gmail.com>
2024-01-18 12:19:50 +0700

    refactor code: countduration.jsx

--- src/pages/CountDuration.jsx
+++ src/pages/CountDuration.jsx
@@ -1,16 +1,34 @@
 import { Link } from "react-router-dom";
 import "../css/countduration.css";
+import { useEffect, useState } from "react";
 
-const COUNTDOWN_TARGET = new Date("2024-03-25T22:55:59");
-console.log("countdown target:", COUNTDOWN_TARGET);
+const COUNTDOWN_TARGET = new Date("2024-01-28T22:55:59");
 
 const getTimeLeft = () => {
     const totalTimeLeft = COUNTDOWN_TARGET - new Date();
-    console.log("total time left: ", totalTimeLeft);
+    // console.log("total time left: ", totalTimeLeft);
+
+    const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((totalTimeLeft / (1000 * 60)) % 60);
+    const seconds = Math.floor((totalTimeLeft / 1000) % 60);
+
+    return { days, hours, minutes, seconds };
 };
-getTimeLeft();
 
 const CountDuration = () => {
+    const [timeLeft, setTimeLeft] = useState({});
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(getTimeLeft());
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
     return (
         <div>
             <Link to="/" className="btn-back">
@@ -18,33 +36,14 @@ const CountDuration = () => {
             </Link>
             <h1 className="header">Count Duration</h1>
             <div className="countdown-container">
-                <div className="box">
-                    <div className="value">
-                        <span>30</span>
+                {Object.entries(timeLeft).map((item, i) => (
+                    <div className="box" key={i}>
+                        <div className="value">
+                            <span>{item[1]}</span>
+                        </div>
+                        <span className="label">{item[0]}</span>
                     </div>
-                    <span className="label">days</span>
-                </div>
-
-                <div className="box">
-                    <div className="value">
-                        <span>10</span>
-                    </div>
-                    <span className="label">hours</span>
-                </div>
-
-                <div className="box">
-                    <div className="value">
-                        <span>20</span>
-                    </div>
-                    <span className="label">minutes</span>
-                </div>
-
-                <div className="box">
-                    <div className="value">
-                        <span>15</span>
-                    </div>
-                    <span className="label">seconds</span>
-                </div>
+                ))}
             </div>
         </div>
     );
